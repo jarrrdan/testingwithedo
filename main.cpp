97e076e06ead3c2e6061e439ecf36f1bc54be6f0
jarrrdan <jordan.s.meyer@outlook.com>
2021-02-22 16:47:38 -0600

    Line test

--- main.cpp
+++ main.cpp
@@ -2,4 +2,4 @@
 
 // edo has typed here and jordan should type somewhere else. maybe he should even fix my typo!
 
-// edo is so lazy! He won't even fix his own typos!!!!
+//Jordan typed at the beginning of the line! edo is so lazy! He won't even fix his own typos!!!!
