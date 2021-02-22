938dc64a89f9336c5df3a1e16cb967f28e62b812
edoardodraetta <edoardoluigi.draetta@studenti.unimi.it>
2021-02-22 23:47:11 +0100

    clash!

--- main.cpp
+++ main.cpp
@@ -2,4 +2,5 @@
 
 // edo has typed here and jordan should type somewhere else. maybe he should even fix my typo!
 
-// edo is so lazy! He won't even fix his own typos!!!!
+// edo is so lazy! He won't even fix his own typos!!!! EDO IS TYPING AT THE END OF THE LINE
+
